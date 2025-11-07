6ae2dae10bcb427f0275e8cb0c1f5ee13f2cbd78
singluntang <cokelight@gmail.com>
2025-11-08 02:25:11 +0800

    docs: Updated ports

--- reactnd-contacts-server2/config.js
+++ reactnd-contacts-server2/config.js
@@ -1,2 +1,2 @@
-exports.port = process.env.REACT_APP_CONTACTS_API_URL || 5001
+exports.port = process.env.REACT_APP_CONTACTS_API_URL || 3001
 exports.origin = process.env.ORIGIN || `http://localhost:${exports.port}`
